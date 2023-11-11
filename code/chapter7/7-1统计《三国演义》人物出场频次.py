ee2aa20cfc3e7c57b14364441e83fe59bfa19315
ldjackie <132369775+ldjackie@users.noreply.github.com>
2023-11-11 22:22:01 +0800

    chapter7_update2

--- code/chapter7/7-1统计《三国演义》人物出场频次.py
+++ code/chapter7/7-1统计《三国演义》人物出场频次.py
@@ -1,6 +1,6 @@
 import jieba
 # 排除词库
-excludes = {"将军", "却说", "荆州", "二人", "不可", "不能", "如此", "商议", "如何", "主公", "军士", "左右", "军马", "引兵", "次日", "大喜", "天下", "东吴", "于是", "今日", "不敢", "魏兵","人马", "陛下", "一人"}
+excludes = {"将军", "却说", "荆州", "二人", "不可", "不能", "如此", "商议", "如何", "主公", "军士", "左右", "军马", "引兵", "次日", "大喜", "天下", "东吴", "于是", "今日", "不敢", "魏兵","人马", "陛下", "一人", "不知"}
 # 以只读方式读取文本内容，文本编码为uft-8
 txt = open("三国演义.txt", "r", encoding="utf-8").read()
 # 用lcut()进行分词
@@ -19,9 +19,11 @@ for word in words:
         rword = "刘备"
     elif word == "孟德" or word == "丞相":
         rword = "曹操"
+    elif word == "都督":
+        rword = "周瑜"
     else:
         rword = word
-        counts[rword] = counts.get(rword, 0) + 1
+    counts[rword] = counts.get(rword, 0) + 1
 # 清洗无用词
 for word in excludes:
     del(counts[word])
